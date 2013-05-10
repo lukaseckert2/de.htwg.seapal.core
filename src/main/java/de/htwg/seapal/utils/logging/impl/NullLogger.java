4b550f4f07c30aaceab6aae189980a476d925962
Benjamin Sautermeister <apps@bsautermeister.de>
2013-05-10 21:07:00 +0200

    Added an implementation for a dummy logger

---
+++ src/main/java/de/htwg/seapal/utils/logging/impl/NullLogger.java
@@ -0,0 +1,27 @@
+package de.htwg.seapal.utils.logging.impl;
+
+import de.htwg.seapal.utils.logging.ILogger;
+
+/**
+ * Silent logger implementation, which can be used if
+ * no logging is required.
+ * @author Benjamin
+ */
+public class NullLogger implements ILogger {
+
+	@Override
+	public void info(String tag, String msg) {
+
+	}
+
+	@Override
+	public void warn(String tag, String msg) {
+
+	}
+
+	@Override
+	public void error(String tag, String msg) {
+
+	}
+
+}
