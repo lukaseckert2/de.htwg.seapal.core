4ee6e0697eb459f7b4a74578878037d74e242002
Patrick Kohan <patrick.kohan@googlemail.com>
2014-01-19 23:52:32 +0100

    last fix

--- src/main/java/de/htwg/seapal/model/impl/SignupAccount.java
+++ src/main/java/de/htwg/seapal/model/impl/SignupAccount.java
@@ -7,10 +7,32 @@ import java.util.UUID;
 
 public final class SignupAccount {
     private String email;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     private String password;
     private UUID id;
     private String firstName;
     private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
     private IAccount account = null;
 
     public SignupAccount() {}
