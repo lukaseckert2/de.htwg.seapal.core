ae72ac1a6f8ee5c340cdfbc7863685e682f7e458
Patrick Kohan <patrick.kohan@googlemail.com>
2014-01-31 17:10:04 +0100

    fixed non working google id login

--- src/main/java/de/htwg/seapal/model/impl/SignupAccount.java
+++ src/main/java/de/htwg/seapal/model/impl/SignupAccount.java
@@ -45,6 +45,7 @@ public final class SignupAccount {
 
     public IAccount getAccount() {
         if (this.account != null) {
+            this.id = account.getUUID();
             return this.account;
         } else {
             IAccount account = new Account();
