4e2995573893536f2ced3bd639f2e7ba0a969e99
Patrick Kohan <patrick.kohan@googlemail.com>
2014-02-13 11:38:53 +0100

    no message

---
+++ src/main/src/test/java/de/htwg/seapal/controller/impl/AccountControllerTest.java
@@ -0,0 +1,96 @@
+package de.htwg.seapal.controller.impl;
+
+import com.google.inject.Inject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class AccountControllerTest {
+
+    @Inject
+    private AccountController controller;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testInjector() throws  Exception {
+        assert(controller != null);
+    }
+
+    @Test
+    public void testCloseDB() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteAccount() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAccounts() throws Exception {
+
+    }
+
+    @Test
+    public void testNewAccount() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAccount() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllAccounts() throws Exception {
+
+    }
+
+    @Test
+    public void testSaveAccount() throws Exception {
+
+    }
+
+    @Test
+    public void testQueryView() throws Exception {
+
+    }
+
+    @Test
+    public void testAuthenticate() throws Exception {
+
+    }
+
+    @Test
+    public void testAccountExists() throws Exception {
+
+    }
+
+    @Test
+    public void testGetByMail() throws Exception {
+
+    }
+
+    @Test
+    public void testGoogleLogin() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPerson() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInternalInfo() throws Exception {
+
+    }
+}
